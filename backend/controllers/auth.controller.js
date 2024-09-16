e95e7fc4d007ac3563297f5a9428806437cae568
Faran Javed <faran1997@outlook.com>
2024-09-16 18:54:29 +0500

    add jwt tokens for signup

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -14,6 +14,30 @@ const generateTokens = (userId) => {
   return { accessToken, refreshToken };
 };
 
+const storeRefreshToken = async (userId, refreshToken) => {
+  await redis.set(
+    `refreshToken:${userId}`,
+    refreshToken,
+    "EX",
+    7 * 24 * 60 * 60
+  ); // 7 days
+};
+
+const setCookies = (res, accessToken, refreshToken) => {
+  res.cookie("accessToken", accessToken, {
+    httpOnly: true, // prevent XSS attacks, cross site scripting attack
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict", // prevents CSRF attack, cross-site request forgery attack
+    maxAge: 15 * 60 * 1000, // 15 minutes
+  });
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true, // prevent XSS attacks, cross site scripting attack
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict", // prevents CSRF attack, cross-site request forgery attack
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+  });
+};
+
 const login = (req, res) => {
   res.send("Login");
 };
@@ -27,9 +51,22 @@ const signup = async (req, res) => {
     }
     const user = await User.create({ name, email, password });
 
-    // send 200 status code for now
-    res.status(201).json({ user, message: "User created successfully" });
-    // TODO: still need to add JWT token
+    // authenticate user and generate tokens
+    const { accessToken, refreshToken } = generateTokens(user._id);
+    await storeRefreshToken(user._id, refreshToken);
+
+    // set cookie
+
+    setCookies(res, accessToken, refreshToken);
+
+    // store refresh token in redis
+
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+    });
   } catch (error) {
     console.log("Error in signup controller", error.message);
     res.status(500).json({ message: error.message });
