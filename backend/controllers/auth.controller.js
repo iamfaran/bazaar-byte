d2ccca569d5f6228fe26d8825cbbc1989a87c196
Faran Javed <faran1997@outlook.com>
2024-09-17 15:11:29 +0500

    add login controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -38,8 +38,29 @@ const setCookies = (res, accessToken, refreshToken) => {
   });
 };
 
-const login = (req, res) => {
-  res.send("Login");
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (user && (await user.comparePassword(password))) {
+      const { accessToken, refreshToken } = generateTokens(user._id);
+      await storeRefreshToken(user._id, refreshToken);
+      setCookies(res, accessToken, refreshToken);
+
+      res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      });
+    } else {
+      res.status(400).json({ message: "Invalid email or password" });
+    }
+  } catch (error) {
+    console.log("Error in login controller", error.message);
+    res.status(500).json({ message: error.message });
+  }
 };
 const signup = async (req, res) => {
   const { email, password, name } = req.body;
