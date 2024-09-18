fefd41ae222028fa58f2c997dc52439b2cafcde6
Faran Javed <faran1997@outlook.com>
2024-09-18 19:00:50 +0500

    product routes setup

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,6 +1,14 @@
 import express from "express";
+import { getAllProducts } from "../controllers/product.controller.js";
 
 const router = express.Router();
+
+// router.get is a method which is used to handle get request
+
+// here we pass 4 parameters to the get method in this case
+// but we can have multiple variable parameters in between
+// however first param will always be the path
+// and last param will always the main function which will be executed when the path is hit
 
 router.get("/", protectRoute, adminRoute, getAllProducts);
 
