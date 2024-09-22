a66c5149d92a9a446e5b7fd44d84726c98d8c26f
Faran Javed <faran1997@outlook.com>
2024-09-22 16:32:46 +0500

    setup basic home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,13 @@
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+
 function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
       </Routes>
