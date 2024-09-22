f78693b7fd0f9bcaa9f03dd6232bf72437a8f980
Faran Javed <faran1997@outlook.com>
2024-09-22 17:36:14 +0500

    add bg

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,12 +5,24 @@ import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/login" element={<LoginPage />} />
-      </Routes>
+    <div className="min-h-screen bg-gray-900 text-white relative overflow-hidden">
+      {/* Background gradient */}
+      <div className="absolute inset-0 overflow-hidden">
+        <div className="absolute inset-0">
+          <div className="absolute top-0 left-1/2 -translate-x-1/2 w-full h-full bg-[radial-gradient(ellipse_at_top,rgba(16,185,129,0.3)_0%,rgba(10,80,60,0.2)_45%,rgba(0,0,0,0.1)_100%)]" />
+        </div>
+      </div>
+
+      {/*  
+           Routes for the pages of the app
+      */}
+      <div className="relative z-50 mt-20">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </div>
     </div>
   );
 }
