249242700660ccec9f8946220c2b1a782c8b68a7
Faran Javed <faran1997@outlook.com>
2024-09-23 20:00:46 +0500

    add signup

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://localhost:5000/", // Replace with your API base URL
+  baseURL: "/api", // Replace with your API base URL
   headers: {
     "Content-Type": "application/json", // Set the default content type
   },
--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import { UserPlus, Mail, Lock, User, ArrowRight, Loader } from "lucide-react";
 import { motion } from "framer-motion";
+import { useUserStore } from "../stores/useUserStore";
 
 const SignUpPage = () => {
   const [formData, setFormData] = useState({
@@ -11,11 +12,12 @@ const SignUpPage = () => {
     confirmPassword: "",
   });
 
+  const { signup, loading } = useUserStore();
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    signup(formData);
   };
-
-  const loading = false;
 
   return (
     <div className="flex flex-col justify-center py-12 sm:px-6 lg:px-8">
