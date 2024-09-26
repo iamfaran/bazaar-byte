91a3d4693fa09386c89551a06278384655dddd10
Faran Javed <faran1997@outlook.com>
2024-09-26 19:13:38 +0500

    add components

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,13 +1,13 @@
 import { ShoppingCart, UserPlus, LogIn, LogOut, Lock } from "lucide-react";
 import { Link } from "react-router-dom";
+import { useUserStore } from "../stores/useUserStore";
+import { useCartStore } from "../stores/useCartStore";
 
 const Navbar = () => {
-  const cart = [];
-  const user = true;
-  const isAdmin = false;
-  const logout = () => {
-    console.log("Logout");
-  };
+  const { user, logout } = useUserStore();
+  const isAdmin = user?.role === "admin";
+  const { cart } = useCartStore();
+
   return (
     <header className="fixed top-0 left-0 w-full bg-gray-900 bg-opacity-90 backdrop-blur-md shadow-lg z-40 transition-all duration-300 border-b border-emerald-800">
       <div className="container mx-auto px-4 py-3">
